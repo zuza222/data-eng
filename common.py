33621ee76c7d0f384ff368f57b377440ecae16ea
zuzab <baczek.zuzia@gmail.com>
2025-04-29 21:08:55 +0200

    ..

---
+++ common.py
@@ -0,0 +1,4 @@
+BRONZE_ROOT = "/home/zunia_linux/Project/studia/data-eng/lab3/lakehouse/bronze"
+SILVER_ROOT = "/home/zunia_linux/Project/studia/data-eng/lab3/lakehouse/silver"
+GOLD_ROOT = "/home/zunia_linux/Project/studia/data-eng/lab3/lakehouse/gold"
+base_path = "/home/zunia_linux/Project/studia/data-eng/lab3/lakehouse"
